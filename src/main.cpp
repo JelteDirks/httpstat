f37ecfe029aefd87035fb68d29c53e51508472cf
Jelte Dirks <j.dirks@outlook.com>
2025-06-21 00:55:12 +0200

    count and print the count
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 #include <fstream>
 #include <istream>
 #include <iostream>
+#include <map>
 #include <string>
-#include <LineReader.h>
 
-int main(int argc, char **argv) {
+int main() {
   std::ifstream header_file ("./var/httpheaders.txt", std::istream::in);
   std::string line;
 
+  std::map<std::string, int> header_counts;
+
   while (std::getline(header_file, line))
   {
-    std::cout << line << std::endl;
+    if (header_counts.find(line) == header_counts.end()) {
+      header_counts.insert(std::make_pair(line, 1));
+    }
   }
 
+  for (auto const& header : header_counts) {
+    std::cout << header.first << ": " << header.second << std::endl;
+  }
 }
